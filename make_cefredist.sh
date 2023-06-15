639b947cb9db5b11c5e7749288f2efe8642b9a58
Miguel Alves <miguel.alves@outsystems.com>
2023-06-15 09:24:58 +0100

    Support CEF ARM64 distribution

--- make_cefredist.sh
+++ make_cefredist.sh
@@ -1,5 +1,10 @@
-TMP="tmp"
-OUTPUT="../package"
+TMP="tmp-$1"
+OUTPUT="../package/cef.redist.$1"
+
+if [ $1 -eq "osx64" ]; then 
+    DOWNLOAD_URL="https://cef-builds.spotifycdn.com/cef_binary_112.3.0%2Bgb09c4ca%2Bchromium-112.0.5615.165_macosx64_minimal.tar.bz2"; else
+    DOWNLOAD_URL="https://cef-builds.spotifycdn.com/cef_binary_112.3.0%2Bgb09c4ca%2Bchromium-112.0.5615.165_macosarm64_minimal.tar.bz2";
+fi
 
 if [ ! -d "$TMP" ]; then
     mkdir "$TMP"
@@ -16,7 +21,7 @@ CEFZIP="cef.tar.bz2"
 CEFBINARIES="cef_binaries"
 if [ ! -f "$CEFZIP" ]; then
     echo "downloading cef binaries"
-    curl -o "$CEFZIP" "https://cef-builds.spotifycdn.com/cef_binary_112.3.0%2Bgb09c4ca%2Bchromium-112.0.5615.165_macosx64_minimal.tar.bz2"
+    curl -o "$CEFZIP" "$DOWNLOAD_URL"
 fi
 
 if [ ! -d "$CEFBINARIES" ]; then
